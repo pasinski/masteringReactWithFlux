d0c5fc79fa5115ee171c9e87a5533f0c2fb853e6
Michal Pasinski <pasinski@gmail.com>
2016-07-04 00:14:33 +0200

    Lot's of changes regarding reflux

---
+++ js/__tests__/views/login-test.jsx
@@ -0,0 +1,8 @@
+/**
+ * Created by michal on 03.07.16.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Login from 'appRoot/views/login'
